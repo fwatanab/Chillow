1a6db561942ce3897c71e24fe4d3a782656a6f7a
fwatanab <fwatanab@student.42tokyo.jp>
2025-06-12 17:04:36 +0900

    .envを引っ張って取得する関数

--- backend/config/config.go
+++ backend/config/config.go
@@ -4,33 +4,34 @@ import (
 	"os"
 )
 
-type	Config struct {
-	JWTSecret			string
-	GoogleClientID		string
-	GoogleClientSecret	string
-	GoogleRedirectURI	string
+type Config struct {
+	JWTSecret           string
+	GoogleClientID      string
+	GoogleClientSecret  string
+	GoogleRedirectURI   string
 
-	DBHost			string
-	DBPort			string
-	DBUser			string
-	DBPassword		string
-	DBName			string
+	DBHost              string
+	DBPort              string
+	DBUser              string
+	DBPassword          string
+	DBName              string
 }
 
-func	LoadConfig() *Config {
-	cfg := &Config{
-		JWTSecret:			os.Getenv("JWT_SECRET"),
-		GoogleClientID:		os.Getenv("GOOGLE_CLIENT_ID"),
-		GoogleClientSecret:	os.Getenv("GOOGLE_CLIENT_SECRET"),
-		GoogleRedirectURI:	os.Getenv("GOOGLE_REDIRECT_URI"),
+var Cfg *Config // グローバルにアクセス可能な設定
 
-		DBHost:		os.Getenv("DB_HOST"),
-		DBPort:		os.Getenv("DB_PORT"),
-		DBUser:		os.Getenv("DB_USER"),
-		DBPassword:	os.Getenv("DB_PASSWORD"),
-		DBName:		os.Getenv("DB_NAME"),
+func LoadConfig() {
+	// 環境変数から構成を読み取る
+	Cfg = &Config{
+		JWTSecret:           os.Getenv("JWT_SECRET"),
+		GoogleClientID:      os.Getenv("GOOGLE_CLIENT_ID"),
+		GoogleClientSecret:  os.Getenv("GOOGLE_CLIENT_SECRET"),
+		GoogleRedirectURI:   os.Getenv("GOOGLE_REDIRECT_URI"),
+
+		DBHost:              os.Getenv("DB_HOST"),
+		DBPort:              os.Getenv("DB_PORT"),
+		DBUser:              os.Getenv("DB_USER"),
+		DBPassword:          os.Getenv("DB_PASSWORD"),
+		DBName:              os.Getenv("DB_NAME"),
 	}
-
-	return cfg
 }
 
