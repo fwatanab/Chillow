16fd88ae977c1c63e4e92d637a879c4057da7711
fwatanab <fwatanab@student.42tokyo.jp>
2025-06-12 17:27:13 +0900

    google oauth2.0ログイン、IDトークンの検証、ユーザーの自動登録、フレンドコードの作成、ユーザー情報のJSON返却、JWT発行

--- backend/main.go
+++ backend/main.go
@@ -4,16 +4,30 @@ import (
 	"log"
 
 	"chillow/config"
+	"chillow/db"
 	"chillow/router"
+	"chillow/model"
 )
 
 func	main() {
-	cfg := config.LoadConfig()                // .env 読み込み
+	// 環境変数の読み込み
+	config.LoadConfig()
 
-	r := router.SetupRouter()       // 全ルーティングを設定
+	// DB接続
+	db.InitDB()
 
-	if err := r.Run(":8080"); err != nil {
-		log.Fatalf("サーバー起動失敗: %v", err)
+	db.DB.AutoMigrate(&model.User{})
+
+	// ルーターの初期化
+	r := router.SetupRouter()
+
+	// サーバー起動
+// 	if err := r.Run(":8080"); err != nil {
+// 		log.Fatalf("❌ サーバー起動失敗: %v", err)
+// 	}
+
+	if err := r.RunTLS(":8443", "./cert/cert.pem", "./cert/key.pem"); err != nil {
+	log.Fatalf("❌ HTTPSサーバー起動失敗: %v", err)
 	}
 }
 
