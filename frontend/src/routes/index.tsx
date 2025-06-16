ad59def3b0d68bf7f709b290bb634ce54888dddf
fwatanab <fwatanab@student.42tokyo.jp>
2025-06-16 15:40:15 +0900

    アプリ全体のルーティングの定義

---
+++ frontend/src/routes/index.tsx
@@ -0,0 +1,21 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Login from '../pages/Login';
+import Home from '../pages/Home';
+import Mypage from '../pages/Mypage';
+import PrivateRoute from './PrivateRoute';
+
+const AppRoutes = () => (
+	<BrowserRouter>
+		<Routes>
+			<Route path="/login" element={<Login />} />
+			<Route path="/" element={
+			 	<PrivateRoute><Home /></PrivateRoute>
+			} />
+			<Route path="/mypage" element={
+			 	<PrivateRoute><Mypage /></PrivateRoute>
+			} />
+		</Routes>
+	</BrowserRouter>
+);
+
+export default AppRoutes;
