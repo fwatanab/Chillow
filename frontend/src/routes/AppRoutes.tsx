845efd4913ecc4ae1dc6fbcb017ceef395ae8ae7
fwatanab <fwatanab@student.42tokyo.jp>
2025-06-26 20:14:06 +0900

    PATH /chat/:id を追加

--- frontend/src/routes/AppRoutes.tsx
+++ frontend/src/routes/AppRoutes.tsx
@@ -2,18 +2,16 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Login from '../pages/Login';
 import Home from '../pages/Home';
 import Mypage from '../pages/Mypage';
+import ChatRoom from '../components/ChatRoom';
 import PrivateRoute from './PrivateRoute';
 
 const AppRoutes = () => (
 	<BrowserRouter>
 		<Routes>
 			<Route path="/login" element={<Login />} />
-			<Route path="/" element={
-			 	<PrivateRoute><Home /></PrivateRoute>
-			} />
-			<Route path="/mypage" element={
-			 	<PrivateRoute><Mypage /></PrivateRoute>
-			} />
+			<Route path="/" element={<PrivateRoute><Home /></PrivateRoute>} />
+			<Route path="/mypage" element={<PrivateRoute><Mypage /></PrivateRoute>} />
+			<Route path="/chat/:id" element={<PrivateRoute><ChatRoom /></PrivateRoute>} />
 		</Routes>
 	</BrowserRouter>
 );
