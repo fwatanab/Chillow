04a76035eada1672ca1ab8b12568fbe921767fd5
fwatanab <fwatanab@student.42tokyo.jp>
2025-06-28 23:40:52 +0900

    gormを使ったメッセージ情報の定義

---
+++ backend/model/message.go
@@ -0,0 +1,14 @@
+package model
+
+import (
+	"time"
+)
+
+type Message struct {
+	ID         uint      `gorm:"primaryKey" json:"id"`
+	SenderID   uint      `json:"sender_id"`
+	ReceiverID uint      `json:"receiver_id"`
+	Content    string    `json:"content"`
+	IsRead     bool      `json:"is_read"`
+	CreatedAt  time.Time `json:"created_at"`
+}
