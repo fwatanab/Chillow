05bdefa5a9a6e21db0578c45318d706acafeb3c8
fwatanab <fwatanab@student.42tokyo.jp>
2025-08-07 22:46:42 +0900

    tailwindcssの設定を追加

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -5,7 +5,19 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}"
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        discord: {
+          background: "#2f3136",
+          sidebar: "#202225",
+          text: "#dcddde",
+          accent: "#5865f2",
+        },
+      },
+      fontFamily: {
+        sans: ['"Noto Sans JP"', 'sans-serif'],
+      },
+    },
   },
   plugins: [],
 }
