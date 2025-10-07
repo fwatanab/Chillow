83f8f13c4f99d806490a7d764388701b8ca4deb9
fwatanab <fwatanab@student.42tokyo.jp>
2025-10-07 13:29:31 +0900

    Friendにfriend_avatar_urlを追加

--- frontend/src/types/friend.ts
+++ frontend/src/types/friend.ts
@@ -4,7 +4,8 @@ export type Friend = {
 	id: number;
 	user_id: number;
 	friend_id: number;
-	user_nickname: string;
+	friend_nickname: string;
+	friend_avatar_url: string | null;
 	created_at: string;
 	updated_at: string;
 };
